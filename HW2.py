bfebc85c1c63bab5e7ab7f8203c843df84ba4d69
Brennlyn <bm128102@student.wpsok.org>
2025-08-22 13:06:39 -0500

    added hw2

---
+++ HW2.py
@@ -0,0 +1,29 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print ('hello world')
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+numbers=4
+big = 'dog'
+food=True
+#3. Print all three variables on the same print function (at the same time).
+print (numbers,big,food)
+#4. Create a variable that asks the user to input an integer.
+dad=int(input("Give me a number between 1 and 10: "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+cool = numbers+dad
+print (cool)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print (cool/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+food = False
+#8. Print the value of the boolean variable.
+print (food)
+#9. Create a variable with a number that contains decimals.
+car = 5.132
+#10. Round the number from #9 up or down using the round function.
+print(round(car))
