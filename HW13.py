fe696249f9b1c3c83e78edf9e91d0e848b0244b6
Brennlyn <bm128102@student.wpsok.org>
2025-11-06 12:47:20 -0600

    added HW12

---
+++ HW13.py
@@ -0,0 +1,11 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW13
+
+#A common exercise in computer science is "FizzBuzz". The rules of
+#the game are simple. Count from 1 to 100 but with every number that
+#is divisible by 3 you say "Fizz" instead of the number,
+#every number divisible by 5 you say "Buzz" instead,
+#and if it's divisible by both you say "FizzBuzz".
+
+#Create a while loop that follows the rules of the game.
