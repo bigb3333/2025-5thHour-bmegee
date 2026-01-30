07a1a88bbb2af8fcc5c6ede119162e5880c7243c
Brennlyn McGee Bonner <bm128102@student.wpsok.org>
2026-01-30 13:18:39 -0600

    Hw18

---
+++ hw18.py
@@ -0,0 +1,41 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW18
+
+import random
+
+#1. Import the "random" library and create a def function that prints "Hello World!"
+def replay():
+    replay = int(input('hello world'))
+    if replay == 1:
+        print('hello world')
+
+#2. Create two empty integer variables named "heads" and "tails"
+int(input())
+int(input())
+#3. Create a def function that flips a coin one hundred times and increments the result in the above variables.
+def replay1():
+    import random
+def replay2():
+    import random
+#4. Call the "Hello world" and "Coin Flip" functions here
+
+replay1()
+replay2()
+#5. Print the final result of heads and tails here
+print()
+#6. Create a list called beanBag and add at least 5 different colored beans to the list as strings.
+beanBag=['blue','red','yellow','green','cyan']
+#7. Create a def function that pulls a random bean out of the beanBag list, prints which bean you pulled, and then removes it from the list.
+def replay3():
+    import random
+    print(beanBag)
+#8. Create a def function that asks if you want to pull another bean out of the bag and, if yes, repeats the #3 def function
+def replay4():
+    import random
+    print()
+
+#9. Call the "Bean Pull" function here
+replay3(beanBag)
+replay4()
+print()
