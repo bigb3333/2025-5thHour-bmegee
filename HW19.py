56a8a884e4ad200a2b599b31b8a2e73e80f7ca35
Brennlyn McGee Bonner <bm128102@student.wpsok.org>
2026-02-23 13:18:51 -0600

    Hw20

---
+++ HW19.py
@@ -0,0 +1,18 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW19
+
+#1. Import the def functions created in problem 1-4 from HW16
+def:
+    
+#2. Call the functions here and run HW19
+
+#3. Delete all calls from HW15 and run HW19 again.
+
+#4. Create a try catch that tries to print variable x (which has no value), but prints "Hello World!" instead.
+
+#5. Create a try catch that tries to divide 100 by whatever number the user inputs, but prints an exception for Divide By Zero errors.
+
+#6. Create a variable that asks the user for a number. If the user input is not an integer, prints an exception for Value errors.
+
+#7. Create a while loop that counts down from 5 to 0, but raises an exception when it counts below zero.
---
+++ HW20.py
@@ -0,0 +1,35 @@
+#Name: Coach Mack
+#Class: 5th Hour
+#Assignment: HW20
+
+#1. Create a class containing a def function that inits self and 3 other attributes for store items (stock, cost, and weight).
+def __init__(self, stock,cost,weight):
+    self.stock = stock
+    self.cost = cost
+    self.weight = weight
+
+#2. Make 3 objects to serve as your store items and give them values to those 3 attributes defined in thE
+"self": "Wireless Mouse",
+    "cost": 25.99,
+    "stock": 50
+  },
+  {
+    "self": "Mechanical Keyboard",
+    "cost": 89.99,
+    "stock": 30
+  },
+  {
+    "self": "USB-C Charger",
+    "cost": 19.99,
+    "stock": 75
+  }
+   {
+
+
+#3. Print the stock of all three objects and the cost of the second store item.
+print()
+#4. Make a def function within the class that doubles the cost an item, double the cost of the second store item, and print the new cost below the original cost print statement.
+def__init__():
+#5. Directly change the stock of the third store item to approx. 1/4th the original stock and then print the new stock amount.
+
+#6. Delete the first store item and then attempt to print the weight of the first store item. Create a try/except catch to fix the error.
